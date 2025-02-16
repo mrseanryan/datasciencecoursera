80de265533be39b39e1708d6113bff1b8d05db13
Sean Ryan <str_ie@yahoo.co.uk>
2025-02-16 11:28:14 +0100

    format

--- Scripts/data-types.R
+++ Scripts/data-types.R
@@ -9,20 +9,20 @@
 
 # vector can contain objects of ONE class
 # list - can contain mix of classes
-v_empty = vector()
+v_empty <- vector()
 print(paste("v_empty", v_empty))
 attributes(v_empty)
-print(paste(1, 1L, Inf, NaN))  # default is 'numeric', 1L is 'integer'
+print(paste(1, 1L, Inf, NaN)) # default is 'numeric', 1L is 'integer'
 class(1)
 class(1L)
 
-print(1/0)
-print(1/Inf)
+print(1 / 0)
+print(1 / Inf)
 
-c1 = complex(real=2,imaginary=3)
+c1 <- complex(real = 2, imaginary = 3)
 print(c1)
 attributes(c1)
-attr(c1,'a-1') <- 1:3
+attr(c1, "a-1") <- 1:3
 attributes(c1)
 print("sqrt of -1")
 print(sqrt(as.complex(-1)))
@@ -31,10 +31,10 @@ print("Creating vectors")
 print(c(0.5, 0.6))
 print(c(TRUE, FALSE))
 print(c(T, F))
-print(c("a","b","c"))
+print(c("a", "b", "c"))
 print(9:29)
-print(c(1+0i, 2+4i))
-print(vector("numeric", length=10))
+print(c(1 + 0i, 2 + 4i))
+print(vector("numeric", length = 10))
 
 print("vectors with mixed types - implicit coercion occurs")
 print(c(1.7, "a"))
@@ -58,11 +58,11 @@ print(as.logical(x))
 # Impossible values (e.g., dividing by zero) are represented by the symbol NaN
 # (not a number).
 print("nonsensical coercion results in NAs")
-print(as.numeric(c("a","b","c")))
-as.complex(c("a","b","c"))
+print(as.numeric(c("a", "b", "c")))
+as.complex(c("a", "b", "c"))
 
 print("lists can have mix of objects")
-l1 <- list(1, "a", TRUE, 1+4i)
+l1 <- list(1, "a", TRUE, 1 + 4i)
 l1
 l1[0] # slicing to an empty list
 l1[1] # slicing to 1st
@@ -76,20 +76,20 @@ dim(m) # matrix has dim(ension) but a vector only has length
 
 print("alt: create a matrix directly from vector, by adding a dimension attribute")
 m <- 1:10
-dim(m) <- c(2, 5)  # setting dimension -> vector becomes a matrix
+dim(m) <- c(2, 5) # setting dimension -> vector becomes a matrix
 m
 
 attributes(m)
-#$dim
-#[1] 2 5 # 2 x rows, 5 x columns
+# $dim
+# [1] 2 5 # 2 x rows, 5 x columns
 
 class(m)
-#[1] "matrix" "array"
+# [1] "matrix" "array"
 
 # matrix with data, via range:
-my_matrix2 <- matrix(data=1:20, nrow=4, ncol=5)
+my_matrix2 <- matrix(data = 1:20, nrow = 4, ncol = 5)
 print("Add a column at left of a matrix - via cbind() - BUT a matrix can only contain 1 type")
-patients <- c('Bill', 'Gina','Kelly', 'Sean')
+patients <- c("Bill", "Gina", "Kelly", "Sean")
 cbind(patients, my_matrix)
 print(" - better to use a data frame, which allows for a mix of types")
 my_data <- data.frame(patients, my_matrix)
@@ -101,23 +101,23 @@ print("alt: create a matrix via column-binding or row-binding")
 x <- 1:3
 y <- 10:12
 cbind(x, y) # column-wise (x then y)
-rbind(x,y) # row-wise (x then y)
+rbind(x, y) # row-wise (x then y)
 
 print("Factors - labelled integer vector - for categorical data")
 # a bit like an enumeration
 ?lm # - fit linear models
 ?glm() # - fit generalized linear models
 
-x <- factor(c("yes","yes","no","yes","no"))
+x <- factor(c("yes", "yes", "no", "yes", "no"))
 table(x) # gives frequency counts of each level (each label)
 unclass(x)
-as.logical( unclass(x) - 1 )
+as.logical(unclass(x) - 1)
 
 print("use levels to set order of the levels - default is alphabetical")
-x <- factor(c("yes","yes","no","yes","no"), levels=c("yes", "no"))
+x <- factor(c("yes", "yes", "no", "yes", "no"), levels = c("yes", "no"))
 x
 unclass(x)
-as.logical( unclass(x) - 1 )
+as.logical(unclass(x) - 1)
 
 print("NA (not avaiable) and NaN (Not a Number)")
 is.na(sqrt(-1))
@@ -126,14 +126,14 @@ is.na(NaN) # NaN is NA, but not vice-versa
 is.nan(NA)
 is.nan(NaN)
 
-0/0 # NaN
+0 / 0 # NaN
 Inf - Inf # NaN
 
-x <- c(1,2,NA, 10, 3)
+x <- c(1, 2, NA, 10, 3)
 x
 is.na(x)
 is.nan(x)
-x <- c(1,2,NaN, NA, 4)
+x <- c(1, 2, NaN, NA, 4)
 is.na(x)
 is.nan(x)
 # note: NA - if used with logical operators -> NA
@@ -154,15 +154,15 @@ class(iris)
 head(data.matrix(iris))
 attributes(iris)
 
-df1 <- data.frame(foo=1:4, bar=c(T,T,F,F))
+df1 <- data.frame(foo = 1:4, bar = c(T, T, F, F))
 df1
 nrow(df1) # count rows
 ncol(df1) # count columns
 attributes(df1)
 data.matrix(df1)
 
-head(x,2) # first 2 rows
-tail(x,2) # last 2 rows
+head(x, 2) # first 2 rows
+tail(x, 2) # last 2 rows
 
 print("Names - R objects can have names -> self-describing objects")
 x <- 1:3
@@ -172,12 +172,12 @@ names(x)
 x
 
 print("List with names")
-x <- list(a=1, b=2, c=3)
+x <- list(a = 1, b = 2, c = 3)
 x
 names(x)
 
 print("matrix with names")
-m <- matrix(1:4, nrow=2, ncol=2)
-dimnames(m) <- list(c("a", "b"), c("c","d"))  # columns, then rows
+m <- matrix(1:4, nrow = 2, ncol = 2)
+dimnames(m) <- list(c("a", "b"), c("c", "d")) # columns, then rows
 m
 dimnames(m)
